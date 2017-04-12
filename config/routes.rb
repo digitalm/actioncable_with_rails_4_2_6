170f24e1b40674321b9f347b0e2d216ae7d67efd
mika <mika.mizuno@gmail.com>
2017-04-12 10:21:30 +0900

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "messages#index"
+  devise_for :users
+  root to: "messages#index"
   resources :messages, only: %i(index create)
-#  mount ActionCable.server => '/cable'
 end
